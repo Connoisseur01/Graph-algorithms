c2aad4e59588c4ce98c483e2f5ad15bd8f551a46
Connoisseur01 <p.polkowski01@gmail.com>
2022-06-05 12:05:49 +0200

    add adjacence list

---
+++ src/List.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include "List.hpp"
+#include <fstream>
+#include <time.h>
+
+void List::read_txt(std::string filename){
+
+    std::ifstream input(filename);
+    if(!input.is_open()){
+        std::cout<<"file did not open";
+        return;
+    }
+
+    
+
+    input >> edges;
+    input >> verticies;
+
+    for(int i = 0; i < verticies; i++){
+        std::vector<Edge> vertex;
+        list.push_back(vertex);
+    }
+
+    int v1, v2, weight;
+
+    for(int i = 0; i < edges; i++){
+        
+        input >> v1;
+        input >> v2;
+        input >> weight;
+        Edge edge;
+        edge.x = v1;
+        edge.y = v2;
+        edge.weight = weight;
+        list[v1].push_back(edge);
+    } 
+    
+
+    input.close();
+}
+
+void List::print(){
+    
+    std::cout<<"edge = (end_vertex, weight)\n\n";
+
+    for(int i = 0; i < verticies; i++){
+        std::cout<< i <<" = [";
+        for(int k = 0; k < list[i].size(); k++){
+            std::cout<<"("<<list[i][k].y<<", "<<list[i][k].weight<<") ";
+        }
+        std::cout<<"]\n";
+    }
+}
+
+void List::generate_random(int edges, int density){
+
+    this->edges = edges;
+
+    list.empty();
+
+    verticies = (edges*100)/density;
+
+    for(int i = 0; i < verticies; i++){
+        std::vector<Edge> vertex;
+        list.push_back(vertex);
+    }
+
+    int v1 = 0, v2 = 0, weight;
+
+    for(int i = 0; i < edges; i++){
+        
+        v1 = rand() % verticies;
+        
+        do{
+            v2 = rand() % verticies;
+        }while(v1 == v2);
+
+        weight = rand() % 30;
+        Edge edge;
+        edge.x = v1;
+        edge.y = v2;
+        edge.weight = weight;
+        list[v1].push_back(edge);
+    } 
+}
+
+std::vector<Edge> List::get_edges(){
+
+    std::vector<Edge> edgeList;
+
+    for(int i = 0; i < verticies; i++){
+        for(int k = 0; k < list[i].size(); k++){
+            Edge e = list[i][k];
+            edgeList.push_back(e);
+        }
+    }
+
+    return edgeList;
+}
--- src/List.hpp
+++ src/List.hpp
@@ -1,13 +1,30 @@
 #pragma once
 #include <iostream>
+#include "Edge.hpp"
+#include <vector>
+
 
 class List{
     public:
 
-        List();
-        ~List();
+       // List();
+       // ~List();
+
+        void read_txt(std::string filepath);
+
+        void print();
+
+        std::vector<Edge> get_edges();
+
+        void generate_random(int vertex_number, int density);
+
+        int get_verticies(){return verticies;}; 
 
     private:
+
+    int edges, verticies;
+
+    std::vector<std::vector<Edge>> list;
 
 
 };
